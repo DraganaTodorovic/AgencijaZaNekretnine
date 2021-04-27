89d3149f2f08d4dbded660e9ce186b731c1fae56
seznalica <dragtod@gmail.com>
2021-04-27 20:39:09 +0200

    dodavanje projekta

---
+++ Agencija/Interfaces/IAgentiRepository.cs
@@ -0,0 +1,17 @@
+﻿using Agencija.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agencija.Interfaces
+{
+    public interface IAgentiRepository
+    {
+        IQueryable<Agent> GetAll();
+        Agent GetById(int id);
+        IQueryable<Agent> GetEkstrem();
+        IQueryable<Agent> GetNajmladji();
+    }
+}
